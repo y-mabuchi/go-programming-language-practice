6d61e5a1b07460b16ad1187d3809024a9780cc38
y-mabuchi <mabu.you@gmail.com>
2021-06-13 11:59:37 +0900

    add q1-10

--- ch1/q1-10/main.go
+++ ch1/q1-10/main.go
@@ -12,10 +12,17 @@ import (
 func main() {
 	start := time.Now()
 	ch := make(chan string)
+	fetchAll(start, os.Args[1:], ch)
+
+	start2 := time.Now()
+	fetchAll(start2, os.Args[1:], ch)
+}
+
+func fetchAll(start time.Time, args []string, ch chan string) {
 	for _, url := range os.Args[1:] {
 		go fetch(url, ch)
 	}
-	for range os.Args[1:] {
+	for range args {
 		fmt.Println(<-ch)
 	}
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
@@ -23,7 +30,7 @@ func main() {
 
 func fetch(url string, ch chan<- string) {
 	start := time.Now()
-	resp, err := http.Get(url)
+	resp,err := http.Get(url)
 	if err != nil {
 		ch <- fmt.Sprint(err)
 		return
@@ -36,5 +43,5 @@ func fetch(url string, ch chan<- string) {
 		return
 	}
 	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes,url)
 }
